83626b78bfab3758a02cfacfb6c7558ee9828767
西田はるか <xitianyaoke@gmail.com>
2020-02-08 20:12:32 +0900

    cannot swipe cells

--- Original2/ViewController.swift
+++ Original2/ViewController.swift
@@ -10,23 +10,39 @@ import UIKit
 import RealmSwift
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var table: UITableView!
     
     var itemList: Results<TodoModel>!
     var item: TodoModel!
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
+        
+        do{
+            let realm = try Realm()
+            itemList = realm.objects(TodoModel.self)
+            table.reloadData()
+        }catch{
+            
+        }
         
         
-        let RealmInstance1 = try! Realm()
-        
-        self.itemList = RealmInstance1.objects(TodoModel.self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        table.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     @IBAction func add(_ sender: Any) {
@@ -44,48 +60,43 @@ class ViewController: UIViewController {
         
         self.table.reloadData()
     }
-
-
+    
+    
 }
 
-extension ViewController: UITableViewDataSource {
-    
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.itemList.count
-    }
-    
+
+          return self.itemList.count
+      }
+      
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
-        
-       item = self.itemList[(indexPath as NSIndexPath).row];
-        
-        cell.textLabel?.text = item.mochimonoteiji
-        
-        return cell
-    }
+          
+          let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "testCell", for: indexPath)
+          
+          item = self.itemList[(indexPath as NSIndexPath).row];
+          
+          cell.textLabel?.text = item.mochimonoteiji
+          
+          return cell
+      }
     
-    //セルの編集許可
-       func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
-       {
-           return true
-       }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCell.EditingStyle.delete {
-//            if let item = item?[indexPath.row] {
-//                try! realm.write {
-//                    realm.delegate(item)
-//                }
-//                tableView.deleteRows(at: <#T##[IndexPath]#>, with: <#T##UITableView.RowAnimation#>)
-//            }
-            
-            
-        }
+    private func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCell.EditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+          
+          if (editingStyle == UITableViewCell.EditingStyle.delete) {
+              do{
+                  let realm = try Realm()
+                  try realm.write {
+                      realm.delete(self.itemList[indexPath.row])
+                  }
+                  table.deleteRows(at: [(indexPath as IndexPath)], with: UITableView.RowAnimation.fade)
+              }catch{
+                
+              }
+            self.table.reloadData()
+          }
     }
-
-
 }
-
-
+    
 
